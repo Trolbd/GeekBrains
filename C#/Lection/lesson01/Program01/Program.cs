124f5b43b9d5a22bf28c5c3d19055e5cd660d874
Alexey Prisukhin <wolasder@gmail.com>
2022-12-03 19:20:44 +0300

    Проверил работу команды dotnet run

--- C#/Lection/lesson01/Program01/Program.cs
+++ C#/Lection/lesson01/Program01/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hexcllo,sffffffffffffdsdssssd World!");
+﻿Console.WriteLine("Hexcllo,sffffffffffffdsdssssd World!");
