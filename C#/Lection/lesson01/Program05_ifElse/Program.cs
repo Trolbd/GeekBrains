5be5ec2d66e17285e9c64b956d46b1f738a11975
Alexey Prisukhin <wolasder@gmail.com>
2022-12-04 19:46:29 +0300

    Ввёл прогу с выбором имён

---
+++ C#/Lection/lesson01/Program05_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+	Console.WriteLine("Ну привет, Дашуля");
+}
+else
+{
+	Console.Write("Здравствуй, ");
+	Console.WriteLine(username);
+}
