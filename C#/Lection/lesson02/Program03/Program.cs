d8eb97518538c3860f5dd7dae91c1f7703c64c54
Alexey Prisukhin <wolasder@gmail.com>
2022-12-05 14:25:40 +0300

    лекция 2

---
+++ C#/Lection/lesson02/Program03/Program.cs
@@ -0,0 +1,44 @@
+﻿// найти элемент массива равный find
+//Console.WriteLine(n); это длина массива
+//Console.WriteLine(array[n-1]); это послед элемент массива
+//Console.WriteLine(array[0]); это 1й
+
+
+
+//int find = 4;
+//Console.Write("Введите число: ");
+//int find = Console.ReadLine();
+//int find = Convert.ToInt32(Console.ReadLine());
+
+
+//Console.WriteLine(find);
+
+
+
+
+//while(index<n)
+//{
+//	Console.WriteLine(index);
+//	if(array[index] == find)
+//	{
+//	Console.WriteLine("index= ", index);
+//		
+//	}
+//index++;
+//}
+
+//Console.WriteLine("Конец программы");
+int[] array = {17,2,3,5,4,99,6,7,99};
+
+int n = array.Length;
+int find = 99;
+int index = 0;
+while(index<n)
+{
+	if(array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;//останавливает работу программы на первом подходящем числе.
+	}
+	index++;
+}
---
+++ C#/Lection/lesson02/Program04/Program.cs
@@ -0,0 +1,49 @@
+﻿// чё то сложное
+void FillArray(int[] collection)
+{
+	int Length = collection.Length;
+	int index = 0;
+	while(index < Length)
+	{
+		collection[index] = new Random().Next(1, 10);
+		index++;
+	}
+}
+
+void PrintArray(int[] col)
+{
+	int count = col.Length;
+	int position = 0;
+	while(position<count)
+	{
+		Console.WriteLine(col[position]);
+		position++;
+	}
+}
+
+int IndexOf(int[] collection, int find)
+{
+	int count = collection.Length;
+	int index = 0;
+	int position =-1;//если ввести слишком большое число программа покажет -1 ак то, что такой элемент ннее найден
+
+	while(index < count)
+	{
+		if(collection[index]==find)
+		{
+		position = index;
+		break;
+		}
+		index++;
+	}
+	return position;
+}
+
+int[] array = new int[10];//создать новый массивв кот будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
