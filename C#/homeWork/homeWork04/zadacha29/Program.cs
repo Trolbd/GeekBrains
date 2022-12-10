f879ea40d6694ce92600c2784983082f00401af7
Alexey Prisukhin <wolasder@gmail.com>
2022-12-10 15:31:47 +0300

    29

--- C#/homeWork/homeWork04/zadacha29/Program.cs
+++ C#/homeWork/homeWork04/zadacha29/Program.cs
@@ -1,11 +1,18 @@
-﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
-Console.WriteLine("Введите элементы массива (без запятых): ");
-int number = int.Parse(Console.ReadLine()!);
+// int[] Array8;
+// Array8 = new int [834564];
+// // Array8[0] = number2[0];
+// System.Console.WriteLine(Array8.Length);
 
-string number2 = number.ToString();
+Console.Write("Введите число (N): ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+int[] array = new int[number];
 
-
-
+for (int i = 0; i < number; i++)
+{
+	array[i] = new Random().Next(number+1);
+	Console.Write(array[i] + "; ");
+}
