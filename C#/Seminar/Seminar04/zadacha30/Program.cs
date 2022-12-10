bf237ea28a3067763d9d38be90153515d849070d
Alexey Prisukhin <wolasder@gmail.com>
2022-12-10 14:12:00 +0300

    прошёл задача 30

--- C#/Seminar/Seminar04/zadacha30/Program.cs
+++ C#/Seminar/Seminar04/zadacha30/Program.cs
@@ -1,2 +1,28 @@
 ﻿// прога выводит массив из 8 элементов, заполненный 0 и 1 в лучайном порядке
 
+int[] GetArray(int length)
+{
+int[] array = new int[length];
+
+Random r = new Random();
+for (int i = 0; i < length; i++)
+{
+array[i] = r.Next(0, 2);
+}
+
+return array;
+}
+
+string GetArrayAsString(int[] array)
+{
+	string result = string.Empty;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+	result += $"{array[i]}, ";
+	}
+
+	return result;
+}
+
+Console.WriteLine(GetArrayAsString(GetArray(int.Parse(Console.ReadLine()!))));
