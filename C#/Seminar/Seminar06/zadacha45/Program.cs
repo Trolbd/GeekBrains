7cf8c7ea0e0527a828669f4fe26e6d2a511e84a3
Alexey Prisukhin <wolasder@gmail.com>
2022-12-13 21:57:37 +0300

    ы

--- C#/Seminar/Seminar06/zadacha45/Program.cs
+++ C#/Seminar/Seminar06/zadacha45/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// создаёт копию заданного массива с помощью поэлементного копирования
+
+int[] GetCopy(int[] array)
+{
+	int[] newArray = new int [array.Length];
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		newArray[i] = array[i];
+	}
+	return newArray;
+}
