54e1ae55b9af3936e43c8dae0d2c5ddea6d22585
Alexey Prisukhin <wolasder@gmail.com>
2022-12-14 18:52:35 +0300

    1

--- C#/Seminar/Seminar06/zadacha45/Program.cs
+++ C#/Seminar/Seminar06/zadacha45/Program.cs
@@ -1,12 +1,12 @@
 ﻿// создаёт копию заданного массива с помощью поэлементного копирования
 
-int[] GetCopy(int[] array)
-{
-	int[] newArray = new int [array.Length];
+// int[] GetCopy(int[] array)
+// {
+// 	int[] newArray = new int [array.Length];
 
-	for (int i = 0; i < array.Length; i++)
-	{
-		newArray[i] = array[i];
-	}
-	return newArray;
-}
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+// 		newArray[i] = array[i];
+// 	}
+// 	return newArray;
+// }
