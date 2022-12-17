db33f058761a7484468315c177931d6ea29a5266
Alexey Prisukhin <wolasder@gmail.com>
2022-12-17 16:54:39 +0300

    d

---
+++ C#/Seminar/Seminar07/zadacha48/Program.cs
@@ -0,0 +1,47 @@
+﻿// задайте двумерный массив размера m на n
+// каждый элемент массива находится по формуле : Аmn = m + n;
+// выведите результат на экран
+
+int[,] inputTwoDimmansionArray(int length1, int length2)
+{
+	int[,] array = new int[length1, length2];
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		FillDimension(i, array);
+	}
+	return array;
+}
+
+int ReadInt()
+{
+	System.Console.WriteLine("Ввод");
+	int a = int.Parse(Console.ReadLine()!);
+	return a;
+}
+
+void FillDimension(int index, int[,] array) //заполнение 2 строки или столбца
+{
+	for (int i = 0; i < array.GetLength(1); i++)
+	{
+		array[index, i] = index + i;
+	}
+}
+
+void GetArrayAsString(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			System.Console.Write($"{array[i, j]} ");
+		}
+		System.Console.WriteLine();
+	}
+}
+
+Task();
+void Task()
+{
+	int[,] array = inputTwoDimmansionArray(ReadInt(), ReadInt());
+	GetArrayAsString(array);
+}
