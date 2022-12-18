0f5b3016c1a0a2e8ab3ce4439f6ac20c2f4477d7
Alexey Prisukhin <wolasder@gmail.com>
2022-12-18 19:58:02 +0300

    52

---
+++ C#/homeWork/homework07/zadacha50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] inputTwoDimmansionArray(int length1, int length2)
+{
+	int[,] array = new int[length1, length2];
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		FillDimension(i, array);
+	}
+	return array;
+}
+
+int ReadInt()
+{
+	System.Console.WriteLine("Ввод");
+	int a = int.Parse(Console.ReadLine()!);
+	return a;
+}
+
+void FillDimension(int index, int[,] array)
+{
+	for (int i = 0; i < array.GetLength(1); i++)
+	{
+		array[index, i] = new Random().Next(1, 10);
+	}
+}
+
+void GetArrayAsString(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			System.Console.Write($"{array[i, j]} ");
+		}
+		System.Console.WriteLine();
+	}
+}
+
+int ReadInt2()
+{
+	System.Console.WriteLine("Ввод искомого числа");
+	int a = int.Parse(Console.ReadLine()!);
+	return a;
+}
+
+int Poisk(int index, int[,] array)
+{
+
+}
+
+
+Task();
+void Task()
+{
+	int[,] array = inputTwoDimmansionArray(ReadInt(), ReadInt());
+	GetArrayAsString(array);
+	
+}
---
+++ C#/homeWork/homework07/zadacha52/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] inputTwoDimmansionArray(int length1, int length2)
+{
+	int[,] array = new int[length1, length2];
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		FillDimension(i, array);
+	}
+	return array;
+}
+
+int ReadInt()
+{
+	System.Console.WriteLine("Ввод");
+	int a = int.Parse(Console.ReadLine()!);
+	return a;
+}
+
+void FillDimension(int index, int[,] array) //заполнение 2 строки или столбца
+{
+	for (int i = 0; i < array.GetLength(1); i++)
+	{
+		array[index, i] = new Random().Next(1, 10);
+	}
+}
+
+void GetArrayAsString(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			System.Console.Write($"{array[i, j]} ");
+		}
+		System.Console.WriteLine();
+	}
+}
+
+double SummArif(int[,] array, int column)
+{
+	int count = 0;
+	for (int i = 0; i < array.GetLength(0); i++)
+		{
+		count = count + array[i, column];
+		}
+	return Math.Round(count / (double) array.GetLength(0),2);
+}
+
+Task();
+void Task()
+{
+	int[,] array = inputTwoDimmansionArray(ReadInt(), ReadInt());
+	GetArrayAsString(array);
+	System.Console.WriteLine();
+		for (int i = 0; i < array.GetLength(1); i++)
+		{	
+			System.Console.WriteLine($"{i} = {SummArif(array, i)}");
+		}
+}
