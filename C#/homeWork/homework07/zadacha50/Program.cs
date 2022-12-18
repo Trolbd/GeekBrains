12b26e1d5e7ebadb3a1fdef4fe6723799a509fdd
Alexey Prisukhin <wolasder@gmail.com>
2022-12-18 20:57:51 +0300

    50

--- C#/homeWork/homework07/zadacha50/Program.cs
+++ C#/homeWork/homework07/zadacha50/Program.cs
@@ -1,60 +1,48 @@
 ﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-int[,] inputTwoDimmansionArray(int length1, int length2)
+int stroka = ReadInt("Введите индекс строки: ");
+int stolb = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[5, 5];
+FillArray(numbers);
+PrintArray(numbers);
+
+if (stroka < numbers.GetLength(0) && stolb < numbers.GetLength(1)) 
 {
-	int[,] array = new int[length1, length2];
-	for (int i = 0; i < array.GetLength(0); i++)
-	{
-		FillDimension(i, array);
-	}
-	return array;
+	Console.WriteLine(numbers[stroka, stolb]);
 }
-
-int ReadInt()
+else 
 {
-	System.Console.WriteLine("Ввод");
-	int a = int.Parse(Console.ReadLine()!);
-	return a;
-}
-
-void FillDimension(int index, int[,] array)
-{
-	for (int i = 0; i < array.GetLength(1); i++)
-	{
-		array[index, i] = new Random().Next(1, 10);
-	}
-}
-
-void GetArrayAsString(int[,] array)
-{
-	for (int i = 0; i < array.GetLength(0); i++)
-	{
-		for (int j = 0; j < array.GetLength(1); j++)
-		{
-			System.Console.Write($"{array[i, j]} ");
-		}
-		System.Console.WriteLine();
-	}
-}
-
-int ReadInt2()
-{
-	System.Console.WriteLine("Ввод искомого числа");
-	int a = int.Parse(Console.ReadLine()!);
-	return a;
-}
-
-int Poisk(int index, int[,] array)
-{
-
+Console.WriteLine($"{stroka}{stolb} такого числа в массиве нет");
 }
 
 
-Task();
-void Task()
+void FillArray(int[,] array)
 {
-	int[,] array = inputTwoDimmansionArray(ReadInt(), ReadInt());
-	GetArrayAsString(array);
-	
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
