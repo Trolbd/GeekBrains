dd1f0a7ba8aa08d8840f4a8f4c06211fd7bd1986
Alexey Prisukhin <wolasder@gmail.com>
2022-12-22 19:17:40 +0300

    dz9

---
+++ C#/homeWork/homeWork09/zadacha64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Number(number);
+
+void Number (int number)
+{
+    if (number < 0)
+	{
+		Console.Write($"{number} не натуральное число");
+	} 
+    if (number == 0)
+	{
+		return;
+	} 
+    Console.Write(number);
+    Number(number - 1);
+}
+
---
+++ C#/homeWork/homeWork09/zadacha66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine()!);
+
+void Summ(int numberM, int numberN, int summ)
+{
+	if (numberM > numberN) 
+	{
+		System.Console.WriteLine(summ);
+		return;
+	}
+	summ = summ + (numberM++);
+	Summ(numberM, numberN, summ);
+}
+
+Summ(numberM, numberN, 0);
